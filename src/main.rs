826b919fe59da555990ab86306ef834c48dd9a63
masha <vichuganovam@gmail.com>
2023-10-24 11:18:30 +0300

    solution

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
+use std::{env, fs};
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+    let result = fs::read_dir(&args[1]);
+
+    if let Err(why) = result {
+        println!("! {}", why.kind());
+    } else if let Ok(paths) = result {
+        for path in paths {
+            if let Ok(entry) = path {
+                let name = entry.file_name();
+                println!("> {}", name.to_string_lossy());
+            }
+        }
+    }
 }
