8bb9bdfb169b909239803288ff9fa2521f6e61b2
masha <vichuganovam@gmail.com>
2024-01-26 03:48:58 +0200

    task 2 solution

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,45 @@
 use std::{env, fs};
 
-fn main() {
-    let args: Vec<String> = env::args().collect();
-    let result = fs::read_dir(&args[1]);
+trait FileFinder {
+    fn find_file(&self, path: String, file_to_find: String);
+}
 
-    if let Err(why) = result {
-        println!("! {}", why.kind());
-    } else if let Ok(paths) = result {
-        for path in paths {
-            if let Ok(entry) = path {
-                let name = entry.file_name();
-                println!("> {}", name.to_string_lossy());
+struct DirSearcher;
+
+impl FileFinder for DirSearcher {
+    fn find_file(&self, path: String, file_to_find: String) {
+        let result = fs::read_dir(path);
+        if let Err(why) = result {
+            println!("! {}", why.kind());
+        } else if let Ok(paths) = result {
+            for path in paths {
+                if let Ok(entry) = path {
+                    let file_name = entry.file_name();
+                    let cur_path = entry.path();
+
+                    if cur_path.is_file() {
+                        if file_to_find.is_empty() {
+                            println!("{}", file_name.to_string_lossy());
+                        }
+                        if file_name.to_string_lossy().to_string() == file_to_find {
+                            println!("File location: {:?}", cur_path);
+                        }
+                    } else {
+                        self.find_file(cur_path.to_string_lossy().to_string(), file_to_find.clone())
+                    }
+                }
             }
         }
+    }
+}
+
+fn main() {
+    let file_searcher = DirSearcher;
+    let args: Vec<String> = env::args().collect();
+
+    match args.len() {
+        2 => file_searcher.find_file(args[1].clone(), String::new()),
+        4 if args[2] == "--find" => file_searcher.find_file(args[1].clone(), args[3].clone()),
+        _ => eprintln!("Usage: program_name <path> [--find <file_name>]"),
     }
 }
